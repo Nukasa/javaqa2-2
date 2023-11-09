29b2ec86d4968cc51d02907c62f149021ce14263
Svetlana Rodionova <nukasa@mail.ru>
2023-11-10 00:25:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+        int startingScore = 200;
+        int replenishment = 1200;
+
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("итог:"+bonus );
+    }
+}
